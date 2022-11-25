4b1dd1658557a9cb14cfa2c18499a2ca445d1331
ryanparketh <ryanpark@rocketmail.com>
2022-11-25 09:23:19 -0500

    added index.js code

--- Develop/models/index.js
+++ Develop/models/index.js
@@ -10,11 +10,21 @@ Product.belongsTo(Category, {
   onDelete: 'CASCADE',
 });
 // Categories have many Products
-
+Category.hasMany(Product, {
+  foreignKey: 'category_id',
+});
 // Products belongToMany Tags (through ProductTag)
-
+Product.belongsToMany(Tag, {
+  // Define the third table needed to store the foreign keys
+  through: ProductTag,
+  foreignKey: 'product_id',
+});
 // Tags belongToMany Products (through ProductTag)
-
+Tag.belongsToMany(Product, {
+  // Define the third table needed to store the foreign keys
+  through: ProductTag,
+  foreignKey: 'tag_id',
+});
 module.exports = {
   Product,
   Category,
